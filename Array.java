82f0b770b9e3163cb593f83466099d0f743895c4
Nandha kumar S <mailstonandhakumar@gmail.com>
2018-08-27 13:56:51 +0530

    Update and rename README.md to Array.java

---
+++ Array.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+  public class Array{
+    public static void main(String args[]){
+      int temp;
+      int a[]={5,4,3,2,1};
+      System.out.println("Sorted array");
+        for(int i=0;i<a.length;i++)
+        {
+          for(int j=i+1;j<a.length;j++)
+          {
+            if(a[i]>a[j])
+            {
+              temp=a[i];
+              a[i]=a[j];
+              a[j]=temp;
+            }
+           } 
+          }
+          for(int i=0;i<a.length;i++)
+          {
+          System.out.println(a[i]);
+          }
+        }
+       } 
