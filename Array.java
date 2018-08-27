fdf4aeafc0179071d5c3cc731eddc66d5dfeefb5
Nandha kumar S <mailstonandhakumar@gmail.com>
2018-08-27 14:10:34 +0530

    Update Array.java

--- Array.java
+++ Array.java
@@ -1,13 +1,24 @@
 import java.io.*;
+import java.util.Scanner;
 
-  public class Array{
+  public class Main{
     public static void main(String args[]){
+      Scanner sc=new Scanner(System.in);
+      
+    
       int temp;
-      int a[]={5,4,3,2,1};
+      int n=sc.nextInt();
+      int a[]=new int[n];
+      for(int i=0;i<n;i++)
+      {
+        a[i]=sc.nextInt();
+      }  
+        
+      
       System.out.println("Sorted array");
-        for(int i=0;i<a.length;i++)
+        for(int i=0;i<n;i++)
         {
-          for(int j=i+1;j<a.length;j++)
+          for(int j=i+1;j<n;j++)
           {
             if(a[i]>a[j])
             {
@@ -17,9 +28,10 @@ import java.io.*;
             }
            } 
           }
-          for(int i=0;i<a.length;i++)
+          for(int i=0;i<n;i++)
           {
-          System.out.println(a[i]);
+          System.out.print(a[i]+" ");
           }
         }
        } 
+
